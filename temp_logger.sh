eb8ea3b14e210040e2f29c98b10f0f48eda2902a
RaymondHughes <raymondbrycehughes@gmail.com>
2026-05-04 15:11:17 -0700

    First Try of Temp Monitor --RH

---
+++ temp_logger.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Activating Temp Logger"
+FILE="temp_log.txt"
+
+head="Time,temp"
+echo $head > $FILE
+
+T=$"F"
+if [[ $1 == "-f" || $1 == "-F" ]]; then
+	T=$"f"
+fi 
+
+while true; do
+	echo 'Monitoring Temperature'
+	#Monitor Time & Temp with python
+	text=$(python tempmonitor.py "$T")
+
+	printf "$text\n" >> $FILE
+
+	sleep 0.1
+done
