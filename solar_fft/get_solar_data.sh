e80c0ccd0755bb5387fdd9e564b03e298bafd154
RaymondHughes <raymondbrycehughes@gmail.com>
2026-05-05 22:46:46 -0700

    Solar Running --RH

--- solar_fft/get_solar_data.sh
+++ solar_fft/get_solar_data.sh
@@ -2,7 +2,7 @@
 echo "Activating Solar Logger"
 FILE_HEAD="solar_log_"
 FILE_TAIL=".txt"
-
+FILE_NUM=1
 head="Time,solar"
 GET_DATA=true
 read -p "Enter Sampling Period (seconds)" PERIOD
@@ -10,18 +10,20 @@ while $GET_DATA; do
 	TIME_ZERO=$(python -c "import time; print(int(time.time()))")
 	LASTTIME=$TIME_ZERO
 	#Make File
-	FILE="$FILE_HEAD$PERIOD$FILE_TAIL"
+	FILE="$FILE_HEAD$FILE_NUM$FILE_TAIL"
 	echo $head > $FILE
 
 	while true; do
-			TIME=$(python -c "import time; print(int(time.time()))")
-		if [[ $((TIME-LASTTIME)) -lt $PERIOD ]]; then
+		echo "getting 30 seconds of data..."
+		TIME=$(python -c "import time; print(int(time.time()))")
+		INTERVAL=$((TIME-LASTTIME))
+		if [[ 1 -eq "$(echo "$INTERVAL<$PERIOD" | bc)" ]]; then
 			continue
 		fi
 		#echo 'Monitoring Solar'
 		#Monitor Time & Solar with python
 		text=$(python solar_monitor.py)
-		printf "$text\n" >> $FILE2
+		printf "$text\n" >> $FILE
 		LASTTIME=$TIME
 		if [[ $((TIME-TIME_ZERO)) -gt 30 ]]; then
 			break
@@ -33,6 +35,7 @@ while $GET_DATA; do
 	if [[ "$PERIOD" == "q" ]]; then
 		echo "quitting" 
 		GET_DATA=false
+		break
 	fi
-
+	FILE_NUM=$((FILE_NUM+1))
 done
